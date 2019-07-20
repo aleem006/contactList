c674da5fade0b2b31de943e2a4460874943f78f9
aleem006 <aleem.mohsan@gmail.com>
2019-07-20 16:16:04 +0500

    first commit layout set

---
+++ app/src/main/java/com/awesometech/android/contactlist/MainActivity.java
@@ -0,0 +1,25 @@
+package com.awesometech.android.contactlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText nameEditText;
+    private EditText lastNameEditText;
+    private EditText phoneEditText;
+    private EditText emailEditText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        nameEditText = findViewById();
+//        lastNameEditText = findViewById();
+//        emailEditText = findViewById(  );
+//        phoneEditText = findViewById();
+    }
+}
